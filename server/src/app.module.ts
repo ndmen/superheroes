2c12755d8120e26a9042e4e5e762b8cd14b67645
Artem Abdullin <artem.abdullin@anoda.mobi>
2022-06-07 23:52:57 +0300

    Added Static Module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,10 +5,15 @@ import { HeroesModule } from './heroes/heroes.module';
 import { FilesModule } from './files/files.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     MongooseModule.forRoot(`${process.env.HMONGODB}`),
     HeroesModule,
     FilesModule,
