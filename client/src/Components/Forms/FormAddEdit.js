f0ea695aa2d91d6f81993818f71973e6edcedf0f
Artem Abdullin <artem.abdullin@anoda.mobi>
2022-06-07 21:17:09 +0300

    Added Client CRUD

---
+++ client/src/Components/Forms/FormAddEdit.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+class AddEditForm extends React.Component {
+  state = {
+    _id: 0,
+    images: '',
+    nickname: '',
+    real_name: '',
+    origin_description: '',
+    superpowers: '',
+    catch_phrase: '',
+  }
+
+  onChange = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  submitFormAdd = e => {
+    e.preventDefault()
+    fetch('http://localhost:4000/heroes', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        images: this.state.images,
+        nickname: this.state.nickname,
+        real_name: this.state.real_name,
+        origin_description: this.state.origin_description,
+        superpowers: this.state.superpowers,
+        catch_phrase: this.state.catch_phrase,
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        if(Array.isArray(item)) {
+          this.props.addItemToState(item[0])
+          this.props.toggle()
+        } else {
+          console.log('failure')
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  submitFormEdit = e => {
+    e.preventDefault()
+    fetch('http://localhost:4000/heroes/' + this.props.item._id, {
+      method: 'patch',
+      enctype: "multipart/form-data",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nickname: this.state.nickname,
+        real_name: this.state.real_name,
+        origin_description: this.state.origin_description,
+        superpowers: this.state.superpowers,
+        catch_phrase: this.state.catch_phrase,
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        if(Array.isArray(item)) {
+          // console.log(item[0])
+          this.props.updateState(item[0])
+          this.props.toggle()
+        } else {
+          console.log('failure')
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount(){
+    // if item exists, populate the state with proper data
+    if(this.props.item){
+      const { _id, images, nickname, real_name, origin_description, superpowers, catch_phrase } = this.props.item
+      this.setState({ _id, images, nickname, real_name, origin_description, superpowers, catch_phrase })
+    }
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.props.item ? this.submitFormEdit : this.submitFormAdd}>
+          <FormGroup>
+          <Label for="images">Nickname</Label>
+          <Input type="file" name="images" id="images" onChange={this.onChange} value={this.state.images === null ? '' : this.state.images} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="nickname">Nickname</Label>
+          <Input type="text" name="nickname" id="nickname" onChange={this.onChange} value={this.state.nickname === null ? '' : this.state.nickname} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="real_name">Real name</Label>
+          <Input type="text" name="real_name" id="real_name" onChange={this.onChange} value={this.state.real_name === null ? '' : this.state.real_name}  />
+        </FormGroup>
+        <FormGroup>
+          <Label for="origin_description">Origin description</Label>
+          <Input type="text" name="origin_description" id="origin_description" onChange={this.onChange} value={this.state.origin_description === null ? '' : this.state.origin_description}  />
+        </FormGroup>
+        <FormGroup>
+          <Label for="superpowers">Superpowers</Label>
+          <Input type="text" name="superpowers" id="superpowers" onChange={this.onChange} value={this.state.superpowers === null ? '' : this.state.superpowers}  />
+        </FormGroup>
+        <FormGroup>
+          <Label for="catch_phrase">Catch phrase</Label>
+          <Input type="text" name="catch_phrase" id="catch_phrase" onChange={this.onChange} value={this.state.catch_phrase === null ? '' : this.state.catch_phrase}  />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
+
+export default AddEditForm
---
+++ client/src/Components/Tables/DataTable.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Table, Button } from 'reactstrap';
+import ModalForm from '../Modals/Modal'
+
+class DataTable extends Component {
+
+  deleteItem = _id => {
+    let confirmDelete = window.confirm('Delete item forever?')
+    if(confirmDelete){
+      fetch('http://localhost:4000/heroes/' + _id, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        _id
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        this.props.deleteItemFromState(_id)
+      })
+      .catch(err => console.log(err))
+    }
+
+  }
+
+  render() {
+
+    const items = this.props.items.map(item => {
+      return (
+        <tr key={item._id}>
+          <td>{item.images}</td>
+          <td>{item.nickname}</td>
+          <td>{item.real_name}</td>
+          <td>{item.origin_description}</td>
+          <td>{item.superpowers}</td>
+          <td>{item.catch_phrase}</td>
+          <td>
+            <div style={{width:"110px"}}>
+              <ModalForm buttonLabel="Edit" item={item} updateState={this.props.updateState}/>
+              {' '}
+              <Button color="danger" onClick={() => this.deleteItem(item._id)}>Del</Button>
+            </div>
+          </td>
+        </tr>
+        )
+      })
+
+      console.log(items)
+
+    return (
+      <Table responsive hover>
+        <thead>
+          <tr>
+            <th>Images</th>
+            <th>Nickname</th>
+            <th>Real name</th>
+            <th>Origin description</th>
+            <th>Superpowers</th>
+            <th>Catch phrase</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items}
+        </tbody>
+      </Table>
+    )
+  }
+}
+
+export default DataTable
